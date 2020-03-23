9c425a6ecfe3c625b272c032c8367a4773149c18
Roy Quesada <rquesada@harper.agency>
2020-03-23 14:49:03 -0600

    init project

---
+++ IceCreamParlor/IceCreamParlor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IceCreamParlor
+//
+//  Created by Roy Quesada on 3/23/20.
+//  Copyright © 2020 Roy Quesada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IceCreamParlor/IceCreamParlorTests/IceCreamParlorTests.swift
@@ -0,0 +1,34 @@
+//
+//  IceCreamParlorTests.swift
+//  IceCreamParlorTests
+//
+//  Created by Roy Quesada on 3/23/20.
+//  Copyright © 2020 Roy Quesada. All rights reserved.
+//
+
+import XCTest
+@testable import IceCreamParlor
+
+class IceCreamParlorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
